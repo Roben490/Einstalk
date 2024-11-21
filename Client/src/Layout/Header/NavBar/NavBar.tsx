2120db10077f63ffbf3cd9bc2bd19ef56add2b59
Roben490 <reuvendagaga@gmail.com>
2024-11-21 15:12:42 +0200

    i added the nav bar to home page

--- Client/src/Layout/Header/NavBar/NavBar.tsx
+++ Client/src/Layout/Header/NavBar/NavBar.tsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 export default function NavBar() {
   return (
     <>
-    <NavLink to={'/'}>EinsTalk</NavLink>
-    <NavLink to={'/addNewPuzzle'}>Add Puzzle</NavLink>
+    <NavLink to={'/'}>EinsTalk </NavLink>
+    <NavLink to={'/addNewPuzzle'}>Add Puzzle </NavLink>
     </>
   )
 }
--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import NavBar from '../Layout/Header/NavBar/NavBar'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <NavBar/>
   )
 }
