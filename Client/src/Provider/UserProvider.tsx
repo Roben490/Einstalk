4bee67cb2924ed6b47d70797e076b7f2e31361ce
yosef-chaim <yoske208@gmail.com>
2024-11-21 12:33:48 +0200

    finish user provider

---
+++ Client/src/Provider/UserProvider.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from 'react'
+import  {IComment,IQuiz,IUser} from "../Interface/Interfaces"
+
+export interface Props {
+    children : React.ReactNode
+}
+
+export interface UserProps {
+    users : IUser[]
+    setUsers : React.Dispatch<React.SetStateAction<IUser[]>>
+}
+
+export const UserConntext = React.createContext<UserProps>({
+    users : [],
+    setUsers : () => {}
+})
+
+
+const UserProvider = ({children}: Props) => {
+    const [users,setUsers] = useState<IUser[]>([])
+  return (
+    <div>
+        <UserConntext.Provider value={{users,setUsers}}>
+            {children}
+
+        </UserConntext.Provider>
+    </div>
+  )
+}
+
+export default UserProvider
