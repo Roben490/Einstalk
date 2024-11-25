2ffb6fbf1668c35304d2c69ace20c840b567a159
kobikru <kobikru@gmail.com>
2024-11-25 15:04:49 +0200

    imges

--- Client/src/pages/About/About.tsx
+++ Client/src/pages/About/About.tsx
@@ -5,24 +5,24 @@ function About() {
   return (
     <div className="card-list">
       <div className="card">
-        <img src="" alt="" />
-        <h1>יוסף חיים מדמון</h1>
-        <h2>{title}</h2>
+        <img className="image-img" src="yosef.jpg" alt="" />
+        <h2>יוסף חיים מדמון</h2>
+        <h4>{title}</h4>
       </div>
       <div className="card">
-        <img src="" alt="" />
-        <h1>יעקב שלמה גרטזנר</h1>
-        <h2>{title}</h2>
+        <img className="image-img" src="yaakov.jpg" alt="" />
+        <h2>יעקב שלמה גרטזנר</h2>
+        <h4>{title}</h4>
       </div>
       <div className="card">
-        <img src="" alt="" />
-        <h1>גד מונסנגו</h1>
-        <h2>{title}</h2>
+        <img className="image-img" src="gadi.jpg" alt="" />
+        <h2>גד מונסנגו</h2>
+        <h4>{title}</h4>
       </div>
       <div className="card">
-        <img src="" alt="" />
-        <h1>ראובן דגגה</h1>
-        <h2>{title}</h2>
+        <img className="image-img" src="reuven.jpg" alt="" />
+        <h2>ראובן דגגה</h2>
+        <h4>{title}</h4>
       </div>
     </div>
   );
